470bcfb9b950185a71bd6bacccbd2eea9a6871c8
Ulemjin Badral <ulemjin@gmail.com>
2021-03-25 22:36:46 -0400

    added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,10 +3,19 @@ var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoName = function() {
+    // grab repo name from url query string
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;        
+        
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
 };
 
 var getRepoIssues = function(repo) {
@@ -23,7 +32,8 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("There was a problem");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
